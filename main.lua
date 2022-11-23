52075331a442cf79a8f0f1b54f36a21b23443575
Rainy0123 <106787899+Rainy0123@users.noreply.github.com>
2022-11-23 17:39:14 +0800

    Update main.lua

--- main.lua
+++ main.lua
@@ -1,11 +1,11 @@
 local Library = loadstring(game:HttpGet("https://pastebin.com/raw/vff1bQ9F"))()
-local Window = Library.CreateLib("Lantern Hub V1| Made by XY#5862", "Sentinel")
+local Window = Library.CreateLib("Lantern Hub V1| Made by XY#5862 | Join Discord server pls", "Sentinel")
 
 --- Aresnal Tab
 local Tab = Window:NewTab("Aresnal")
 local Section = AresnalTab:NewSection("Aimbot")
 Section:NewButton("Aimbot (Buggy)", "Enable Aimbot.", function()
-    print("Clicked")
+    loadstring(game:httpget('https://pastebin.com/raw/Qiv0XkRq')()
 end)
 
 elseif game.PlaceId == 3956818381 then
