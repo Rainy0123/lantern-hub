9a47dfc731a47dd52b6de426e21f44babe361a3d
Rainy0123 <106787899+Rainy0123@users.noreply.github.com>
2022-11-23 13:13:33 +0800

    Create main.lua

---
+++ main.lua
@@ -0,0 +1,12 @@
+local Library = loadstring(game:HttpGet("https://pastebin.com/raw/vff1bQ9F"))()
+local Window = Library.CreateLib("TITLE", "Sentinel")
+
+--- Aresnal Tab
+local Tab = Window:NewTab("Aresnal")
+local Section = AresnalTab:NewSection("Aimbot")
+Section:NewButton("Aimbot (Buggy)", "Enable Aimbot.", function()
+    print("Clicked")
+end)
+
+elseif game.PlaceId == 3956818381 then
+loadstring(game:HttpGet("https://pastebin.com/raw/vff1bQ9F"))()
